22cd77825439f1cbe162474f1486e8b792ec6c91
Zorigbold  Munkh-Erdene <zemunkh@gmail.com>
2019-12-07 01:25:53 +0800

    Scan logging into file is done.

--- lib/helper/file_manager.dart
+++ lib/helper/file_manager.dart
@@ -3,19 +3,21 @@ import 'dart:convert';
 import 'package:intl/intl.dart';
 import 'package:path_provider/path_provider.dart';
 
-bool jsonExists = false;
-File jsonRegisterFile;
-Directory dir;
-Map<String, int> fileContent;
+
 
 class FileManager {
   static void saveScanData(String productCode, bool matched, DateTime currentDate) {
-    String filename = '${DateFormat("yyyyMMdd").format(currentDate)}.json';
+    String filename = '${DateFormat("yyyyMMdd").format(currentDate)}';
     String time = DateFormat("yyyy/MM/dd HH:mm:ss").format(currentDate);
     print('Time: $time');
 
-    writeJson(productCode, 1, filename);
+    String matching = matched ? 'matched' : 'unmatched';
 
+    writeJson(productCode, filename).then((_){
+      writeToCsv(filename, time, productCode, matching);
+    });
+
+    // create csv and add logging data:
   }
 
   static Future<String> get _getLocalPath async {
@@ -23,20 +25,39 @@ class FileManager {
     return directory.path;
   }
 
-  static Future<File> getFile(String filename) async {
+  static Future<File> getJsonFile(String filename) async {
     final path = await _getLocalPath;
-    File jsonFile = File("$path/$filename");
-    if(!await jsonFile.exists()) {
-      return jsonFile;
+    File file = File("$path/$filename.json");
+    if(!await file.exists()) {
+      print('Creating new file');
+      await file.create(recursive: true);
+      Map<String, dynamic> content = {'Counter': 0, 'filename': filename};
+      file.writeAsStringSync(json.encode(content));
+      print('Json Data: $content');
+      return file;
     } else {
-      jsonFile.createSync();
-      return jsonFile;
+      print('Opening existing file!');
+      // file.deleteSync(recursive: true);
+      return file;
     }
   }
 
-  static Future<Null> writeJson(String key, int quantity, String filename) async {
-    final file = await getFile(filename);
-    Map<String, int> content = {key: quantity};
+  static Future<File> getCsvFile(String filename) async {
+    final path = await _getLocalPath;
+    File file = File("$path/$filename.csv");
+    if(!await file.exists()) {
+      print('Creating CSV file');
+      file.createSync(recursive: true);
+      return file;
+    } else {
+      print('Opening Existing CSV file');
+      return file;
+    }
+  }
+
+  static Future<Null> writeJson(String key, String filename) async {
+    final file = await getJsonFile(filename);
+    Map<String, dynamic> content = {key: 0, 'filename': filename};
 
     
     Map<String, dynamic> jsonFileContent = json.decode(file.readAsStringSync());
@@ -51,63 +72,28 @@ class FileManager {
       }
     }
  
-
     print('Json Data: $jsonFileContent');
     file.writeAsStringSync(json.encode(jsonFileContent));
   }
 
-  static Future<String> readFromFile(String filename) async {
+  static Future<String> getCounter(String filename, String key) async {
     try {
-      final file = await getFile(filename);
-      String contents = json.decode(file.readAsStringSync());
-      return contents;
+      final file = await getJsonFile(filename);
+      Map<String, dynamic>  contents = json.decode(file.readAsStringSync());
+      return contents[key].toString();
     } catch(e) {
-      return "e";
+      return "Error";
     }
   }
+
+  static Future<Null> writeToCsv(String filename, String time, String key, String matching) async {
+    final file = await getCsvFile(filename);
+    
+    String countedValue = await getCounter(filename, key);
+    String newData = '$time, $key, $countedValue, $matching \r\n';
+
+    String content = file.readAsStringSync();
+    file.writeAsStringSync(content + newData);
+    print(content);
+  }
 }
-
-
-
-
-// Future<Null> _registerBarcode(String filename) async{
-//   Map<String, int> fileContent;
-
-//   dir = await getApplicationDocumentsDirectory();
-//   jsonRegisterFile = File("${dir.path}/$filename");
-//   jsonExists = jsonRegisterFile.existsSync();
-//   if(!jsonExists) {
-//     await jsonRegisterFile.create(recursive: true);
-//   } else {
-//     fileContent = json.decode(jsonRegisterFile.readAsStringSync());
-//   }
-// }
-
-// Future<Null> createFile(Map<String, int> content, Directory dir, String filename) async{
-//   print('Creating file...');
-//   dir = await getApplicationDocumentsDirectory();
-//   File file = new File("${dir.path}/$filename");
-//   file.createSync();
-//   jsonExists = true;
-//   file.writeAsStringSync(json.encode(content));
-// }
-
-// void writeToJsonFile(String key, int quantity, String filename) {
-//   print('Writing to file');
-//   Map<String, int> content = {key: quantity};
-//   if(jsonExists) {
-//     print('File does exist!');
-//     Map<String, int> jsonFileContent = json.decode(jsonRegisterFile.readAsStringSync());
-//     if(jsonFileContent[key] != null) {
-//       jsonFileContent[key] = jsonFileContent[key] + quantity;
-//     } else {
-//       jsonFileContent.addAll(content);
-//     }
-//     jsonRegisterFile.writeAsStringSync(json.encode(jsonFileContent));
-//   } else {
-//     print('File does not exist');
-//     createFile(content, dir, filename);
-//   }
-//   fileContent = json.decode(jsonRegisterFile.readAsStringSync());
-//   print('Json Data: $fileContent');
-// }
