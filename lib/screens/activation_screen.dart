687eb0adef51415632d86916cca11f5f3e67ac38
Zorigbold  Munkh-Erdene <zemunkh@gmail.com>
2019-12-07 23:31:13 +0800

    Activation page is added

--- lib/screens/activation_screen.dart
+++ lib/screens/activation_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../styles/theme.dart' as Style;
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 
@@ -9,10 +10,14 @@ class ActivationScreen extends StatelessWidget {
   final _activationController = TextEditingController();
   final FocusNode _activationNode = FocusNode();
 
+  Future<Null> _focusNode(BuildContext context, FocusNode node) async {
+    FocusScope.of(context).requestFocus(node);
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    Widget _activationInput(String hintext, TextEditingController _controller, FocusNode currentNode) {
+    Widget _activationInput(String labeltext, TextEditingController _controller, FocusNode currentNode) {
       return Stack(
         alignment: const Alignment(1.0, 1.0),
         children: <Widget>[
@@ -20,67 +25,108 @@ class ActivationScreen extends StatelessWidget {
             child: TextFormField(
               style: TextStyle(
                 fontSize: 22, 
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
               ),
               decoration: InputDecoration(
                 filled: true,
-                fillColor: Color(0xFF004B83),
-                hintText: hintext,
-                hintStyle: TextStyle(
-                  color: Colors.white, 
-                  fontWeight: FontWeight.w200,
+                fillColor: Colors.white,
+                labelStyle: Theme.of(context).textTheme.display1,
+                labelText: labeltext,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
               ),
-              autofocus: true,
+              autofocus: false,
+              keyboardType: TextInputType.number,
               controller: _controller,
               focusNode: currentNode,
               onTap: () {
-                // _focusNode(context, currentNode);
+                _focusNode(context, currentNode);
               },
             ),
-          ),
-          FlatButton(
-            onPressed: () {
-              // _clearTextController(context, _controller, currentNode);
-              // if(_controller == _masterController) {
-              //   _productController.clear();
-              // }
-            },
-            child: Icon(EvaIcons.closeOutline, color: Colors.white, size: 30,),
           ),
         ],
       );
     }
 
 
+    final logoStyle = TextStyle(
+      fontFamily: 'QuickSand',
+      fontWeight: FontWeight.w500,
+      color: Colors.grey[700],
+      fontSize: 36.0,
+    );
+
+    final logo = Card(
+      elevation: 0,
+      color: Style.Colors.background,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Image.asset('assets/images/barcode-icon.png'),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              'Retail',
+              style: logoStyle,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Text(
+              'Scanner',
+              style: logoStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+
+    final button = Padding(
+      padding: EdgeInsets.all(10),
+      child: MaterialButton(
+        onPressed: () {
+          String inputText = _activationController.text;
+          print('$inputText');
+        },
+        child: Text(
+          'Activate',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'QuickSand',
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+        shape: StadiumBorder(),
+        color: Colors.blue,
+        splashColor: Colors.teal,
+        height: 55,
+        minWidth: 350,
+        elevation: 2,
+      )
+    ); 
+
+    final mainView = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        logo,
+        Padding(
+          padding: EdgeInsets.all(10),
+          child: _activationInput('Activation input', _activationController, _activationNode),
+        ),        
+        button,
+      ], 
+    );  
+
     return Scaffold(
       // appBar: AppBar(
       //   title: Text('Others Page'),
       // ),
+      backgroundColor: Style.Colors.background,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Card(
-              child: Image.asset('assets/images/barcode-icon.png'),
-
-            ),
-            Text(
-              'Activation',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.blue,
-                fontSize: 25.0,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: _activationInput('Please insert Activation code', _activationController, _activationNode),
-            )
-            
-          ], 
-        ),
+        child: mainView,
       ),
     );
   }
